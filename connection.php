1337670cc7dfb12327572ce2bc5f129c5549e29f
Tej Pratap Sngh <tejpratap46@gmail.com>
2016-05-29 17:08:05 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -10,12 +10,12 @@
 
 
   //hostinger
- /*
+ 
   $hostname = "mysql.hostinger.in";
-  $database = "u790027247_store";
-  $user = "u790027247_store";
-  $password = "9860637720";
- */
+  $database = "	u807693745_nfcst";
+  $user = "u807693745_nfcst";
+  $password = "prashant";
+ 
 
 // freehostingnoads
 /*$user = "u230326583_onest";
@@ -23,12 +23,12 @@ $password = "9860637720";
 $database = "u230326583_onest";
 $hostname = "mysql.freehostingnoads.net";*/
 
-
+/*
 $user = "root";
 $password = "";
 $database = "tej";
 $hostname = "localhost";
-
+*/
 
 mysql_connect ( $hostname, $user, $password );
 @mysql_select_db ( $database ) or die ( "Unable to select database" );
